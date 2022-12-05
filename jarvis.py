c2c9f1f5368e9bdede97798ef18239242ab2a7d5
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-05 00:22:58 -0500

    added some comment

--- jarvis.py
+++ jarvis.py
@@ -2,12 +2,16 @@ from openai_api import *
 from text_to_speech import *
 from speech_to_text import *
 
-
+# Record audio and turn it to text
 print('Type enter once you finish to record.')
 input_text = sp_to_txt()
 print(f'The recorded text was : {input_text}')
+
+# Send the text to ChatGPT and return his output text
 print('Sending it to ChatGPT now')
 text = chatGPT(input_text)
+
+#Turn ChatGPT output text into speech
 print(f'Here is the response :')
 print(text)
 txt_to_sp(text)
